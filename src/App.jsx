68dd9706f5b4bb689f18f35bfef06a49454d6e82
Howard404 <Gh031800#>
2023-12-07 14:35:15 -0800

    Desktop navigation created

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ import Navigation from './components/Navigation'
 function App() {
   return (
     <>
-      <Navigation />
-      <h1>Rice Band Site</h1>
-      <img src="src/assets/hero.jpeg" alt="hero image" width={700}/>
+
+      <Navigation />  
+
+      {/* Keep main tag */}
+      <main> 
+
+        {/* Remove content in main and add Hero component */}
+        <h1 style={{textAlign: 'center'}}>Rice Band Site</h1>
+        <div className="remove-later" style={{textAlign: 'center'}}>
+          <img src="src/assets/hero.jpeg" alt="hero image" width='40%'/>
+        </div>
+
+      </main>
     </>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,38 @@
 // import { useState } from "react";
 import './syles/navigation.css';
+import '../index.css';
 import darkMode from '../assets/dark.svg';
 import lightMode from '../assets/sun.svg';
+import { useState } from 'react';
 
 export default function Navigation() {
-    // JS
+    
+    const [displayMode, setDisplayMode] = useState(false);
 
+    function toggleMode() {
+        setDisplayMode(!displayMode);
+        handleToggleEvent(displayMode);
+    }
 
+    function handleToggleEvent(modeValue) {
+        if(modeValue){
+            document.querySelector('main').style.background = '#0F0F0F';
+            document.querySelector('main').style.color = '#FFFFF9';
+        }else {
+            document.querySelector('main').style.background = '#FFFFF9';
+            document.querySelector('main').style.color = '#0F0F0F';
+        }
+    }
 
-    // JSX
+    // JSX RENDERED PAGE
     return (
         <div className="nav_bar">
             <div className="nav_container">
 
-                <div className="icon_container logo">Rice Band <span><img src={lightMode} alt="dark mode" width={21}/></span></div>
+                <div className="icon_container logo">Rice Band <span><img src={displayMode ? lightMode : darkMode} alt="dark mode" width={20} onClick={toggleMode}/></span></div>
 
-                <div className="menu_items menu_items_mobile">            
+                <div className="menu_items menu_items_mobile">  
+
                     <ul className="menu">
                         <li>
                             <a href="#">Home</a>
@@ -33,12 +50,13 @@ export default function Navigation() {
                             <a href="#">Newletter</a>
                         </li>
                     </ul>
-                    <div className="hamburger_container">
-                        {/* Position Hamburger as absolute */}
-                        <input type='checkbox' className="hamburger"></input>
-                    </div>
-                </div>
 
+                    <div className="hamburger_container">
+                            {/* Position Hamburger as absolute */}
+                            <input type='checkbox' className="hamburger"></input>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
