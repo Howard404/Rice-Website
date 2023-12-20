b5c616beacbbae8ecfe4f7ad61ea47e8288acece
Howard404 <Gh031800#>
2023-12-19 17:23:17 -0800

    Marquee faster speed plus font wieght

--- src/views/MarqueeSection.jsx
+++ src/views/MarqueeSection.jsx
@@ -4,7 +4,7 @@ import './styles/marquee.css'
 export default function MarqueeSection() {
   return (
     <div className="marquee-container">
-      <Marquee autoFill speed={90}>Rice Band - Music - Tour - News -&nbsp;</ Marquee>
+      <Marquee autoFill speed={130}>Rice Band - Music - Tour - News -&nbsp;</ Marquee>
     </div>
   )
 }
