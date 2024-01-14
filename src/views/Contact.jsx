21ec66a0f0adfa09a202d6ae31df97bbfea4cf9f
Howard404 <Gh031800#>
2024-01-13 17:07:17 -0800

    Contact page styling, no functionality

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -29,7 +29,7 @@ export default function Contact() {
     <div className='contact-page'>
 
       <div className="contact-heading">
-        <h1>Book Us Now!</h1>
+        <h1>Book a Show!</h1>
       </div>
 
       <div className='contact-container'>
