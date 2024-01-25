6bf69df4d244223a1c44ef336f4585593ce0617e
Howard404 <Gh031800#>
2024-01-25 15:53:16 -0800

    Responsive design plus color
    Hero section text and image are now responsive to all device.

--- src/views/hero.jsx
+++ src/views/hero.jsx
@@ -9,7 +9,7 @@ function hero() {
                 <img src="/images/hero.jpeg" width='80%' className='hero-image'/>
                 <h1 className='hero-header-left'></h1>
             </div>
-            <h1 className='hero-header-bottom'>All American Rock & Roll</h1>
+            <h1 className='hero-header-bottom'><i>All American Rock & Roll</i></h1>
         </div>
     );
 }
