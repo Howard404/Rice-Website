c1d38efc62be784316bc21545dbb82dc3c23748f
Grant-Howard509 <howardgrant120@gmail.com>
2024-04-21 23:30:06 -0700

    Social media border-radius

--- src/views/Social.jsx
+++ src/views/Social.jsx
@@ -5,7 +5,7 @@ import './styles/social.css'
 export default function Social() {
   return (
     <div className="social-container">
-        <a href="https://music.apple.com/us/artist/rice/1489563617" className="apple"><FontAwesomeIcon icon={faApple} className="social-link apple"/></a>
+        <a href="https://music.apple.com/us/artist/rice/1489563617" className="apple-link"><FontAwesomeIcon icon={faApple} className="social-link apple"/></a>
         <a href="https://www.instagram.com/rice_trio/"><FontAwesomeIcon icon={faInstagram} className="social-link instagram"/></a>
         {/* <a href="https://www.facebook.com/ricethyband"><FontAwesomeIcon icon={faFacebook} className="social-link facebook"/></a> */}
         <a href="https://open.spotify.com/artist/252nRH19GUk132CXhLIOID"><FontAwesomeIcon icon={faSpotify} className="social-link spotify"/></a>
