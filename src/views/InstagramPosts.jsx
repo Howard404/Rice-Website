ca243eb008e2ffd11cd5ee631ea0c4201e4d0bd7
Grant-Howard509 <howardgrant120@gmail.com>
2024-04-21 23:54:11 -0700

    Instagram headers

--- src/views/InstagramPosts.jsx
+++ src/views/InstagramPosts.jsx
@@ -4,6 +4,8 @@ import { InstagramEmbed } from 'react-social-media-embed';
 export default function InstagramPosts() {
   return (
     <div className='instagram-container'>
+        <h1>Gram</h1>
+        <h3>If you want...</h3>
         <div className='instagram-content'>
                 <InstagramEmbed url="https://www.instagram.com/rice_trio/" className='instagram-post'/>
         </div>
